44b938d357c945540bfcb742a0b4864a1ad0d2aa
Vanya <vanyailnitsk@yandex.ru>
2022-02-11 00:10:32 +0300

    MaxStrOfLetter

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-for (let i=0;i<5;i++){
-    console.log(i);
+let str="AABBBBBBBBBBCCCCCCDDDDDEEEDFGDG";
+console.log(str.length);
+let MaxCount="";
+let count="";
+for (let i=0;i<str.length-1;i++){
+    if (str[i]==str[i+1]){
+        count=count.concat(str[i]);
+        if (str[i+1]!=str[i+2]){
+            count=count.concat(str[i+1]);
+        }
+        if (count.length>MaxCount.length){    
+            MaxCount=count;
+        }
+    }       
+    else {count="";}
 }
-
+console.log(MaxCount);
